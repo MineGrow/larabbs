4cd1720d0c49e57d120d69a474f598f62eabed0f
vagrant <vagrant@homestead>
2018-01-22 10:02:52 +0000

    Only authorzed user can edit user profile

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    public function __construct()
+    {
+        // 中间件名称，要进行过滤的动作
+        $this->middleware('auth', ['except' => ['show']]);
+    }
     /**
      * 个人页面展示
      * @param User $user
@@ -26,11 +31,13 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
         $data = $request->all();
         if ($request->avatar) {
             $result = $uploader->save($request->avatar, 'avatars', $user->id, 362);
---
+++ app/Policies/UserPolicy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class UserPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * 判断是否为当前用户
+     * @param User $currentUser
+     * @param User $user
+     * @return bool
+     */
+    public function update(User $currentUser, User $user)
+    {
+        return $currentUser->id === $user->id;
+    }
+}
